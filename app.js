4acf75f542038aa58b336dc4b6cad73f9c2540ca
Billiemo <mokirpolk@gmail.com>
2024-08-20 12:41:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
 const express = require('express');
 const app = express();
+const middleware = require('./middleware');
 
-// Middleware to handle redirection based on user-agent
-app.use((req, res, next) => {
-  const userAgent = req.headers['user-agent'];
-  console.log('User-Agent:', userAgent); // سجل قيمة user-agent
-  
-  if (userAgent && userAgent.includes('facebookexternalhit')) {
-    console.log('Redirecting to external URL'); // سجل إذا تم تنفيذ إعادة التوجيه
-    res.redirect('https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb');
-  } else {
-    next();
-  }
+// Apply middleware
+app.use(middleware.logger); // Log all requests
+app.use(express.json()); // Parse JSON in requests
+app.use(middleware.userAgentRedirect); // Handle facebookexternalhit user-agent
+
+// Define a primary route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-// Handle other routes
-app.get('*', (req, res) => {
-  res.send('billie eilish !');
-});
-
-// Middleware for handling errors
+// Middleware for error handling
 app.use((err, req, res, next) => {
   console.error('Unhandled error:', err.stack);
   res.status(500).send('Something went wrong!');
 });
 
+// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
