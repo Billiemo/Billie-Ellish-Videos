1258d5c7f1cbfe00353dd96761e38da7860c40e1
Billiemo <mokirpolk@gmail.com>
2024-08-20 13:15:44 +0100

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,11 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Redirect all requests to the target URL
 app.get('*', (req, res) => {
-    try {
-        res.redirect(301, 'https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb');
-    } catch (error) {
-        console.error('فشلت عملية إعادة التوجيه:', error);
-        res.status(500).send('خطأ في الخادم الداخلي');
-    }
+    res.redirect(301, 'https://cleopatraadulatefrench.com/yrbajwpw4?key=c13dd6d2f97a5a0f967c85445d720bbb');
 });
 
 app.listen(PORT, () => {
-    console.log(`الخادم يعمل على المنفذ ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
